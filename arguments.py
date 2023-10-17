ce6d97c0f20d3d1722ea8a0fcd7f86d6b0a31bf6
GmanStak <836375453@qq.com>
2023-10-17 09:12:41 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -5,5 +5,5 @@ def get_args():
     parser.add_argument("-a","--ipaddress",type=str,default='localhost')
     parser.add_argument("-p","--port",type=int,default=9119)
     parser.add_argument("-c","--config",type=str,default="./conf/domain.json")
-    args = 解析器。解析参数（）
-    返回参数
+    args = parser.parse_args()
+    return args
